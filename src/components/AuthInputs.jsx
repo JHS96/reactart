b11d0362208d1cc4fd5ebaa3369675e9624a5ecd
JHS96 <jschaper@hotmail.co.za>
2025-05-15 14:00:24 +0200

    Scoped css styles via css modules

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -23,7 +23,9 @@ export default function AuthInputs() {
   return (
     <div id='auth-inputs'>
       <div className='controls'>
-        <p>
+        <p className='paragraph'>
+          {' '}
+          {/* paragraph style won't be applied here due to scoping via css modules */}
           <label className={`label ${emailNotValid ? 'invalid' : ''}`}>
             Email
           </label>
