d8866e669be8d10e5831d95cd7e3a65b2a448dca
JHS96 <jschaper@hotmail.co.za>
2025-05-15 13:00:10 +0200

    Inline conditional styling - NOT ideal

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -21,32 +21,38 @@ export default function AuthInputs() {
   const passwordNotValid = submitted && enteredPassword.trim().length < 6;
 
   return (
-    <div id="auth-inputs">
-      <div className="controls">
+    <div id='auth-inputs'>
+      <div className='controls'>
         <p>
           <label>Email</label>
           <input
-            type="email"
-            className={emailNotValid ? 'invalid' : undefined}
+            type='email'
+            style={{ backgroundColor: emailNotValid ? '#fed2d2' : '#d1d5db' }}
+            // className={emailNotValid ? 'invalid' : undefined}
             onChange={(event) => handleInputChange('email', event.target.value)}
           />
         </p>
         <p>
           <label>Password</label>
           <input
-            type="password"
-            className={passwordNotValid ? 'invalid' : undefined}
+            type='password'
+            style={{
+              backgroundColor: passwordNotValid ? '#fed2d2' : '#d1d5db',
+            }}
+            // className={passwordNotValid ? 'invalid' : undefined}
             onChange={(event) =>
               handleInputChange('password', event.target.value)
             }
           />
         </p>
       </div>
-      <div className="actions">
-        <button type="button" className="text-button">
+      <div className='actions'>
+        <button type='button' className='text-button'>
           Create a new account
         </button>
-        <button className='button' onClick={handleLogin}>Sign In</button>
+        <button className='button' onClick={handleLogin}>
+          Sign In
+        </button>
       </div>
     </div>
   );
